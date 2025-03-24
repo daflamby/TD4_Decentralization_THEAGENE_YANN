d25d0745635ef6fc358c94930e843deb6770ae53
daflamby <157586163+daflamby@users.noreply.github.com>
2025-03-24 03:14:35 +0100

    Update jestSetup.ts

--- __test__/config/jestSetup.ts
+++ __test__/config/jestSetup.ts
@@ -1,18 +1,1 @@
-import request from "supertest"; 
-import { simpleOnionRouter } from "C:/Users/yanna/TD4_Decentralization_THEAGENE_YANN/src/onionRouters/simpleOnionRouter.ts";
-import { BASE_ONION_ROUTER_PORT } from "C:/Users/yanna/TD4_Decentralization_THEAGENE_YANN/src/config.ts";
-import { Server } from "http";
 
-let server: Server | undefined;
-
-beforeAll(async () => {
-  // Wait for the promise to resolve and get the Server instance
-  server = await simpleOnionRouter(0);
-});
-
-afterAll(() => {
-  // Ensure server.close() is called only after the server has been initialized
-  if (server) {
-    server.close();
-  }
-});
