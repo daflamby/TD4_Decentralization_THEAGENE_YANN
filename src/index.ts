8d054095504a99dad15dc7e451a770d8f9e4cbac
daflamby <157586163+daflamby@users.noreply.github.com>
2025-03-24 03:10:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-import { launchOnionRouters } from "./onionRouters/launchOnionRouters";
-import { launchRegistry } from "./registry/registry";
-import { launchUsers } from "./users/launchUsers";
+import { launchNetwork } from ".";
 
-export async function launchNetwork(nbNodes: number, nbUsers: number) {
-  // launch node registry
-  const registry = await launchRegistry();
-
-  // launch all nodes
-  const onionServers = await launchOnionRouters(nbNodes);
-
-  // launch all users
-  const userServers = await launchUsers(nbUsers);
-
-  return [registry, ...onionServers, ...userServers];
+function main() {
+  launchNetwork(10, 2);
 }
+
+main();
