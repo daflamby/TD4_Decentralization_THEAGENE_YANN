8674543ccd2392ad2f34fc3e831db1ef0d2854b2
daflamby <157586163+daflamby@users.noreply.github.com>
2025-03-27 18:27:10 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import { launchOnionRouters } from "./onionRouters/launchOnionRouters";
 import { launchRegistry } from "./registry/registry";
 import { launchUsers } from "./users/launchUsers";
+// index.ts
+
+import { WebCrypto } from 'webcrypto-liner';
+
+// Crée une instance de WebCrypto
+const webCrypto = new WebCrypto();
+
+// Ajoute le polyfill globalement
+globalThis.crypto = webCrypto;
+
+// Exemple d'utilisation de l'API Web Crypto
+const generateKey = async () => {
+  const key = await crypto.subtle.generateKey(
+    {
+      name: "AES-GCM",
+      length: 256,
+    },
+    true, // La clé peut être exportée
+    ["encrypt", "decrypt"]
+  );
+
+  console.log("Clé générée :", key);
+};
+
+// Appel de la fonction pour générer la clé
+generateKey().catch((error) => console.error("Erreur:", error));
+
 
 export async function launchNetwork(nbNodes: number, nbUsers: number) {
   // launch node registry
