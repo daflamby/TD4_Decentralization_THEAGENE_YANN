233cf9b7dc911496d85b9f06d1881090fe75a22a
daflamby <157586163+daflamby@users.noreply.github.com>
2025-03-27 16:25:12 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -1,12 +1,8 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
-import * as crypto from "../crypto";  
-export type Node = {
-   nodeId: number; 
-   pubKey: string; 
-   prvKey?: string; 
-  };
+
+export type Node = { nodeId: number; pubKey: string };
 
 export type RegisterNodeBody = {
   nodeId: number;
@@ -17,44 +13,31 @@ export type GetNodeRegistryBody = {
   nodes: Node[];
 };
 
+let nodeRegistry: Node[] = [];
+
 export async function launchRegistry() {
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
-  let nodes: Node[] = []; // Initialize nodes as an empty array of type Node[]
 
-
-  _registry.get("/getPrivateKey", (req, res) => { 
-    const { nodeId } = req.query;
-    const node = nodes.find(node => node.nodeId === Number(nodeId));
-    if (node && node.prvKey) {
-      res.json({ result: node.prvKey });
-    } else {
-      res.status(404).send('Node not found or private key unavailable.');
-    }
-  });
-
-  _registry.get("/getNodeRegistry", (req, res) => {
-    const publicNodes = nodes.map(({ nodeId, pubKey }) => ({ nodeId, pubKey }));
-    res.json({ nodes: publicNodes });
-  });
-    // TODO implement the status route
   _registry.get("/status", (req, res) => {
-    res.send("live"); 
+    res.status(200).send("live");
   });
 
-  _registry.post("/registerNode",async (req, res) =>  {
-    const { nodeId, pubKey } = req.body;
-    // For testing: Generate a key pair, export the private key, and replace the public key with the provided one
-    const { publicKey, privateKey } = await crypto.generateRsaKeyPair();
-    const exportedPrvKey = await crypto.exportPrvKey(privateKey);
-    nodes.push({ nodeId, pubKey, prvKey: exportedPrvKey || "" }); // Store the private key for testing purposes
-    console.log(`Registering node ${nodeId} with public key`);
-    res.status(200).json({ result: "Node registered successfully" });
+  // Implement the registerNode route
+  _registry.post("/registerNode", (req: Request, res: Response) => {
+    const { nodeId, pubKey } = req.body as RegisterNodeBody;
+    nodeRegistry.push({ nodeId, pubKey });
+    res.status(200).send("Node registered successfully");
+  });
+
+  // Implement the getNodeRegistry route
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.status(200).json({ nodes: nodeRegistry });
   });
 
   const server = _registry.listen(REGISTRY_PORT, () => {
-    console.log(`registry is listening on port ${REGISTRY_PORT}`);
+    console.log(`Registry is listening on port ${REGISTRY_PORT}`);
   });
 
   return server;
